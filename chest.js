4652ee04c836882cbca5e271fbd834a31e2fdb66
oleobal <olivier.contrib@leobal.eu>
2018-10-09 16:30:54 +0200

    Reorganize things

---
+++ chest.js
@@ -0,0 +1,18 @@
+/**
+ * Contains things to generate actual lists from parameters
+ * 
+ * Do import loot.js before this file !
+ */
+
+
+/**
+ * Contents include weapons, gold, and precious items
+ * 
+ */
+function getChestContents(value)
+{
+	// decide what part of the total value to allocate to what
+	var wpval = getRandom(0,100)
+	var itval = getRandom(0,100-itval)
+	var gdval = 100-wpval-itval
+}
