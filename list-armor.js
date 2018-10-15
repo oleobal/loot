7b6415db19dd1186c9765285595352f00d5096cd
oleobal <olivier.contrib@leobal.eu>
2018-10-15 14:53:22 +0200

    Add emoji
    I do what I want

---
+++ list-armor.js
@@ -0,0 +1,52 @@
+/**
+ * armors
+ *
+ */
+
+var armors = {
+items = [
+	{
+		name:"chainmail",
+		dmg:4,
+		dmgspread:2,
+		val:60,
+		dexmod:-1,
+		desc:"A set of riveted chain mail that perfectly contours one's body.",
+		weight:10,
+		"materials":{"steel":[100, 0, 1000]},
+		"commonness":[50, 0, 1000]
+	}
+],
+
+badMods = [
+	{
+		"name":"",
+		"dmg":1,
+		"dmgspread":1,
+		"val":1,
+		"dexmod":,
+		"desc":"",
+		"matermust":null,
+		"catmust":null,
+		"catcant":null,
+		"wpcant":null,
+		"commonness":[100, 0, 1000]
+	},
+],
+
+goodMods = [
+	{
+		"name":"",
+		"dmg":1,
+		"dmgspread":1,
+		"dexmod":1,
+		"val":1,
+		"desc":"",
+		"matermust":null,
+		"catmust":null,
+		"catcant":null,
+		"wpcant":null,
+		"commonness":[100, 0, 1000]
+	},
+],
+}
